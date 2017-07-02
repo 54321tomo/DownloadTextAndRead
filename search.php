d644be2b35ff17b6bb524d41c1f0bbb3e0b1de63
54321tomo <t-reeve2000@hotmail.com>
2017-07-02 16:55:15 +0100

    Main Search Page
    Where you initially search from

---
+++ search.php
@@ -0,0 +1,20 @@
+<html>
+
+  <head>
+    <title>Search a Text File!</title>
+  </head>
+  
+  <body>
+  
+    <form action="results.php" method="GET">
+      <table>
+        <tr><td>Username:</td><td><input type="text" name="q" value="Enter the username you're looking for"/></td></tr>
+        <tr><td></td><td><input type="submit" value="Search"/></td></tr>
+      </table>
+    </form>
+    
+    <b>#LeftStylesToYou</b>
+  
+  </body>
+
+</html>
